563b8ac6be2f8a637a88dcf1a9cd3d7aceecae9c
saachivar <saachi.varshney@gmail.com>
2024-07-28 01:42:40 -0400

    navbar exists

--- foodiez2.0-frontend/src/Components/NavBar.js
+++ foodiez2.0-frontend/src/Components/NavBar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/NavBar.css'
 
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/saved-recipes">Saved Recipes</Link></li>
-        {/* Add more links as needed */}
-      </ul>
-    </nav>
+    <div className='nav-container'>
+        <nav className="navbar">
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/saved-recipes">Saved Recipes</Link></li>
+            {/* Add more links as needed */}
+        </ul>
+        </nav>
+    </div>
   );
 };
 
