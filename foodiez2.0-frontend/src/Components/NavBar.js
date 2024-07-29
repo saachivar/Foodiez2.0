955737057bc070e66069acefb6fed9310476ef7e
saachivar <saachi.varshney@gmail.com>
2024-07-29 01:41:19 -0400

    starting home page

--- foodiez2.0-frontend/src/Components/NavBar.js
+++ foodiez2.0-frontend/src/Components/NavBar.js
@@ -5,14 +5,22 @@ import '../css/NavBar.css'
 
 const Navbar = () => {
   return (
-    <div className='nav-container'>
-        <nav className="navbar">
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/saved-recipes">Saved Recipes</Link></li>
-            {/* Add more links as needed */}
+    <div className="nav-container">
+      <nav className="navbar">
+        <div className="navbar-brand">foodiez</div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/saved-recipes" className="nav-link">Saved Recipes</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/sign-in" className="nav-link">Sign In</Link>
+          </li>
+          {/* Add more links as needed */}
         </ul>
-        </nav>
+      </nav>
     </div>
   );
 };
--- foodiez2.0-frontend/src/Home.js
+++ foodiez2.0-frontend/src/Home.js
@@ -1,21 +1,37 @@
 import { Link } from 'react-router-dom';
 import NavBar from './Components/NavBar';
+import './css/Home.css'
 
 
 export default function Home() {
 
   return (
-        <div className='Home'>
+        <div>
         <NavBar />
-          <div className='Intro'>
-            <div className='Welcome'>
+          <div className='home-container'>
+            <div className='home'>
+              <div className='top-half'>
+                <div className='enter-ing'>
+                  <form className='enter-ing-form'>
+                    <div className="form-group">
+                      <label>Enter the ingredients to your custom recipe:</label>
+                      <input type="text" />
+                    </div>
+                  </form>
+                </div>
+                <div className='or'>
+                  OR
+                </div>
+                <div className='paste-link'>
+                  <form>
+                    <div className="form-group">
+                      <label>Paste a link to a recipe here:</label>
+                      <input type="text" />
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
-            <div className='Desc'>
-            </div>
-          </div>
-          <div className='Disclaimer'>
-            <p>
-            </p>
           </div>
         </div>
 
